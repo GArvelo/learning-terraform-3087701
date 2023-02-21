dcb9152f4de8c60f3e9663e5527faf08c307ba5d
Geraldo Arvelo <119348946+GArvelo@users.noreply.github.com>
2023-02-21 11:11:22 -0500

    Create dev vpc

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-3c"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
@@ -49,7 +49,7 @@ module "blog_sg" {
   
   name   = "blog"
   vpc_id = module.vpc.public_subnets[0]
-  
+
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules        = ["all-all"]
