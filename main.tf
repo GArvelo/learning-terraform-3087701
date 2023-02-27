c8c460edca7c0b455a6118f59de55320f20a3303
Geraldo Arvelo <119348946+GArvelo@users.noreply.github.com>
2023-02-27 13:29:33 -0500

    Create dev vpc

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_instance" "blog" {
 }
 
 module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
+  source  = "terraform-aws-module/security-group/aws"
   
   version     = "4.17.1"
   description = "Creating security group module"
@@ -53,7 +53,7 @@ module "blog_sg" {
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  
+
   egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
 }
