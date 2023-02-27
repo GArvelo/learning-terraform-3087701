02195bcf95e704741efdfbb10a32c4057aa9f283
Geraldo Arvelo <119348946+GArvelo@users.noreply.github.com>
2023-02-27 13:15:05 -0500

    Create dev vpc

--- main.tf
+++ main.tf
@@ -44,10 +44,11 @@ vpc_security_group_ids = [module.blog_sg.security_group_id]
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.17.1"
+  
+  version     = "4.17.1"
   description = "Creating security group module"
-  name   = "blog"
-  vpc_id = module.vpc.public_subnets[0]
+  name        = "blog"
+  vpc_id      = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
