cc737e485cb23768a028bff71841825533e69fd9
Geraldo Arvelo <119348946+GArvelo@users.noreply.github.com>
2023-02-28 10:52:07 -0500

    load

--- main.tf
+++ main.tf
@@ -36,19 +36,57 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
  
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+ vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "Learning Terraform"
   }
 }
 
+module "alb"{
+  source = "terraform-aws-module/alb/aws"
+  version = "~> 6.0"
+
+  name = "blog-alb"
+
+  load_balanceer_type= "application"
+
+  vpc_id = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+  security_groups =module.blog_sg.security_group_id
+
+  traget_groups = [
+    {
+      name_prefix = "blog-"
+      backend_protocol = "HTTP"
+      backend_port = 80
+      target_type = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
+    }
+  ]
+}
+  http_tcp_listeners = [
+    {
+      port = 80
+      protocol = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
     version     = "4.17.1"
 
   name        = "blog"
-  vpc_id      = module.vpc.public_subnets
+  vpc_id      = module.blog_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
