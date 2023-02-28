d36de4742a97cee492fe248877801bf398529abe
Geraldo Arvelo <119348946+GArvelo@users.noreply.github.com>
2023-02-28 11:08:07 -0500

    load

--- main.tf
+++ main.tf
@@ -17,7 +17,6 @@ data "aws_ami" "app_ami" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
   version = "3.19.0"
-
   name = "dev"
   cidr = "10.0.0.0/16"
 
@@ -36,6 +35,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
  
+ subnet_id              = module.vpc.public_subnets[0]
  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
