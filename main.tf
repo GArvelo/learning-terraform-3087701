47ff77852ea8160abf1433143825641aa42b92b9
Geraldo Arvelo <119348946+GArvelo@users.noreply.github.com>
2023-02-28 11:03:36 -0500

    load

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ module "alb"{
   subnets         = module.blog_vpc.public_subnets
   security_groups = module.blog_sg.security_group_id
 
-  traget_groups = [
+  target_groups = [
     {
       name_prefix      = "blog-"
       backend_protocol = "HTTP"
